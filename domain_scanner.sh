15b5f4d97c85ff78338e4df10e4dbf6bed4acd26
matticamp <2312425+matticamp@users.noreply.github.com>
2023-09-01 17:17:52 -0400

    Update domain_scanner.sh
    replaced fierce with amass

--- domain_scanner.sh
+++ domain_scanner.sh
@@ -1,58 +1,79 @@
 #!/bin/bash
+# Domain recon script created by Mattia Campagnano on Fri Dec 6 14:17:49 EST 2019.
 
-# Domain recon script created by Mattia Campagnano on Fri Dec 6 14:17:49 EST 2019
+# This Bash script reads a domain from the user, it runs nslookup, dig, whois and host 
+# commands and also attempts a zone transfer on the supplied domain through dnsrecon.
+# Finally, the script will run a subdomain enumeration with amass on the user-provided domain.
 
- # This Bash script reads a domain from the user, it runs nslookup, dig, whois and host 
-#commands and also attempts a zone transfer on the supplied domain through dnsrecon and fierce.
-
-echo -n "Please enter a domain to check:  "
+echo -n "Please enter a domain to check:  ";
 
 read domain
 
-# Ask user to indicate a series of output files, one for each command to be run
-echo -n "Enter organization name:  "
+# Ask user to indicate a series of output files, one for each command to be run.
+# If you don't specify a full filepath, all output files will be created in the current working directory.
 
-read org
+echo -n "Output file 1 -nslookup: "
+read output1
 
-echo -n "Enter pentester name: "
+echo -n "Output file 2 -dig: "
+read output2
+
+echo -n "Output file 3 - whois: "
+read output3
+
+echo -n "Output file 4 -host: "
+read output4
+
+echo -n "Ouput file 5 - dnsrecon -d: "
+read output5
+
+echo -n "Output file 6 - dnsrecon -d -t: "
+read output6
+
+echo -n "Output file 7 - amass: "
+read output7
+
+echo 
+
+echo -n "Enter consultant name: "
 
 read name
 
 today=$(date)
 
-echo "Output of the nslookup command run on ${domain} performed by ${name} on ${today}" >> $org-nslookup.txt
+echo "Output of the nslookup command run on ${domain} performed by ${name} on ${today}" >> "$output1"
 
-echo "Output of the dig command run on ${domain} performed by ${name} on ${today}" >> $org-dig.txt
+echo "Output of the dig command run on ${domain} performed by ${name} on ${today}" >> "$output2"
 
-echo "Output of the whois command run on ${domain} performed by ${name} on ${today}" >> $org-whois.txt
+echo "Output of the whois command run on ${domain} performed by ${name} on ${today}" >> "$output3"
 
-echo "Output of the host command run on ${domain} performed by ${name} on ${today}" >> $org-host.txt
+echo "Output of the host command run on ${domain} performed by ${name} on ${today}" >> "$output4"
 
-echo "Output of the dnsrecon command run on ${domain} performed by ${name} on ${today}" >> $org-dnsrecon.txt
+echo "Output of the dnsrecon command run on ${domain} performed by ${name} on ${today}" >> "$output5"
  
-echo "Output of the dnsrecon command with zone transfer run on ${domain} performed by ${name} on ${today}" >> $org-dnsrecon1.txt
+echo "Output of the dnsrecon command with zone transfer run on ${domain} performed by ${name} on ${today}" >> "$output6"
 
 
-echo "Output of the fierce command run on ${domain} performed by ${name} on ${today}" >> $org-fierce.txt
+echo "Output of the amass command run on ${domain} performed by ${name} on ${today}" >> "$output7"
 
-nslookup $domain >> $org-nslookup.txt &
-
-
-dig $domain >> $org-dig.txt &
+nslookup $domain >> $output1 &
 
 
-whois $domain >> $org-whois.txt &
+dig $domain >> $output2 &
 
 
-host $domain >> $org-host.txt &
+whois $domain >> $output3 &
 
 
-dnsrecon -d $domain >> $org-dnsrecon.txt &
+host $domain >> $output4 &
 
 
-dnsrecon -d $domain -t axfr >> $org-dnsrecon1.txt &
+dnsrecon -d $domain >> $output5 &
 
 
-fierce --domain $domain >> $org-fierce.txt &
+dnsrecon -d $domain -t axfr >> $output6 &
 
-echo "All commands successfully performed. Exiting..."
+
+amass enum -d $domain >> $output7 &
+
+echo "All commands successfully performed. Exiting...
